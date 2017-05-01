8743d1bd62ac6362f89f0305459dd7e17aea5e24
jthompson5 <mr_joe_thompson5@hotmail.com>
2017-05-01 21:04:58 +0100

    Changes to Stock
    Made it so stock can’t change price if its not traceable

--- StockMarket/src/Stock.java
+++ StockMarket/src/Stock.java
@@ -9,7 +9,7 @@
  * @author user
  */
 public class Stock {
-    
+
     private static int count = 0;
     int stockID;
     String companyName;
@@ -21,6 +21,7 @@ public class Stock {
 
     /**
      * Construct stock
+     *
      * @param companyName The company the stock belongs to.
      * @param price The price of the stock.
      */
@@ -29,14 +30,15 @@ public class Stock {
         this.companyName = companyName;
         this.type = type;
         this.price = price;
-        if(price > 0) {
+        if (price > 0) {
             tradeable = true;
         }
         this.sharesOffered = sharesOffered;
     }
-    
+
     /**
      * Sets the stockID
+     *
      * @param stockID The stockID.
      */
     public void setStockID(int stockID) {
@@ -45,70 +47,81 @@ public class Stock {
 
     /**
      * Increase value of the stock
+     *
      * @param value The value to increase the stock by.
      */
     public void increaseValue(int value) {
-        if(checkPositiveValue(value)) {
-            price += value;
-        }     
+        if (tradeable) {
+            if (checkPositiveValue(value)) {
+                price += value;
+            }
+        }
     }
 
     /**
      * Decrease value of the stock
+     *
      * @param value The value to decrease the stock by.
      */
     public void decreaseValue(int value) {
-        if(checkPositiveValue(value)) {
-           price -= value; 
-        }
-        if(this.price <= 0) {
-            makeStockUntradeable();
-            this.price = 0;
+        if (tradeable) {
+            if (checkPositiveValue(value)) {
+                price -= value;
+            }
+            if (this.price <= 0) {
+                makeStockUntradeable();
+                this.price = 0;
+            }
         }
     }
-    
+
     /**
      * Retrieve the value of the stock
+     *
      * @return The value of the stock.
      */
-    public double getValueOfStock(){
+    public double getValueOfStock() {
         return price;
     }
 
     /**
      * Retrieve the company the stock retrieves
+     *
      * @return The company name.
      */
     public String getCompanyStockRepresents() {
         return companyName;
     }
-    
+
     /**
      * Sets company stock's name
+     *
      * @param companyName The company name of the stock.
      */
     public void setCompanyStockReperesents(String companyName) {
         this.companyName = companyName;
     }
-    
+
     /**
      * Checks if the value passed in is positive or not
+     *
      * @param value The value to check if it's positive.
      * @return If value is greater than zero.
      */
     public boolean checkPositiveValue(int value) {
         return value > 0;
     }
-    
+
     /**
      * Makes the stock untradeable
      */
     public void makeStockUntradeable() {
         this.tradeable = false;
     }
-    
+
     /**
      * Changes the type of the stock
+     *
      * @param type The type the stock will be changed to.
      */
     public void changeStockType(StockType type) {
